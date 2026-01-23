f606c8a2d742d19976bf3185770776cefed74ff4
shelbykane <shelby.sorenson@gmail.com>
2026-01-23 16:10:24 -0600

    Testing to see if this works?

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        System.out.println("Hi I'm just trying to figure out how to get this all set up");
+
+    }
+}
