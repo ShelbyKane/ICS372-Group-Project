714eb785f82e815007f8c476263e472806458b8f
MadisonYurich <nosidamhciruy99@gmail.com>
2026-01-27 16:11:24 -0600

    Item class filled out

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,5 +1,53 @@
 package org.example;
-
+//**
 public class Item {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Item(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    /**
+     * getters and setters
+     *
+     * please note that the setters for price and
+     * name might not be needed later on
+     **/
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTotal() {
+        return quantity * price;
+    }
+    @Override
+    public String toString() {
+        return "Item{" + "name=" + name + ", quantity=" + quantity + ", price=" + price + '}';
+    }
 
 }
