07501f5e1c31e2a00785cf08879cb65ab5222e51
Osob <osob.mm@gmail.com>
2026-03-24 03:08:02 -0500

    added automatic backup of imported JSON and XML to backupOrders with duplicate prevention

--- src/main/java/org/example/AutoImporter.java
+++ src/main/java/org/example/AutoImporter.java
@@ -1,13 +1,32 @@
 package org.example;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 public class AutoImporter {
     public static java.util.ArrayList<String> imported_files = new java.util.ArrayList<>();  // store names of imported files to avoid duplicates
+
+    public static void copy_to_backup(File file) {
+        try {
+            File backupDir = new File("backupOrders");
+            if (!backupDir.exists()) {
+                backupDir.mkdir();
+            }
+            File newFile = new File(backupDir, file.getName());
+            Files.copy(file.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Copied to backup: " + file.getName());
+        }
+        catch (Exception e) {
+            System.out.println("Backup copy failed");
+        }
+    }
 
     public static void read_folder(String folderName, OrderList order_list) {
     File importFolder = new File(folderName);
         if (!importFolder.exists() || importFolder.listFiles() ==null){
             return;
         }
+
     File[] importFiles = importFolder.listFiles(); // get all da files
 
     if (importFiles != null) { // check folder existance and is not empty
@@ -21,11 +40,13 @@ public class AutoImporter {
             if (file.getName().endsWith(".json")) {  // import JSON files
                 System.out.println("Importing JSON file");
                 order_list.import_json_file(file.getPath());
+                copy_to_backup(file);
             }
 
             else if (file.getName().endsWith(".xml")) {  // import XML files
                 System.out.println("Importing XML file");
                 order_list.import_xml_file(file.getPath());
+                copy_to_backup(file);
             }
         }
     }
