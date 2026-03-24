2fbf94f4b02cfe0cb0d2c629c4d3d054fe7ad880
b5sitikesi <likychenc@gmail.com>
2026-03-24 16:08:40 -0500

    Removed unused packages.

--- src/main/java/org/example/ExtraMethods.java
+++ src/main/java/org/example/ExtraMethods.java
@@ -1,14 +1,5 @@
 package org.example;
 
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 public class ExtraMethods {
     public static String create_filename(String id) {
         return "order_" + id + ".xml";
