a99d93c5f24b24ed0e88d695f0ecd3039937b10a
b5sitikesi <likychenc@gmail.com>
2026-03-24 04:30:27 -0500

    A class used to read files from a directory.

---
+++ src/main/java/org/example/ReloadFiles.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.DirectoryIteratorException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ReloadFiles {
+    public static void start_reload(OrderList orders, String path) {
+        Path dir = Path.of(path);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path file : stream) {
+                orders.import_xml_file(file.toString());
+            }
+        } catch (IOException | DirectoryIteratorException x) {
+            System.out.println("ReloadFiles error 1");
+        }
+
+    }
+}
