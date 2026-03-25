860b87cc4f10f98171cf26d1563bf74b6ea1a8fb
Carlos <crdandrews@gmail.com>
2026-03-25 05:13:51 -0500

    Add OrderTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,12 @@ repositories {
 dependencies {
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.14.0")
+//    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
