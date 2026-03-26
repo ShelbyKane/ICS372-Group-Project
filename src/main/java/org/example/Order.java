d51f02141a902a5a91b7baca0c2616de8db951c7
shelbykane <shelby.sorenson@gmail.com>
2026-03-25 21:11:11 -0500

    Fixed total cost issue

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -176,7 +176,7 @@ public class Order {
         for (Item i : items) {
             sb.append(i.stringForOrder());
         }
-        sb.append("\nTotal cost of order: " + getTotalCost());
+        sb.append(String.format("\nTotal cost of order: %.2f", getTotalCost()));
 
         return String.valueOf(sb);
     }
